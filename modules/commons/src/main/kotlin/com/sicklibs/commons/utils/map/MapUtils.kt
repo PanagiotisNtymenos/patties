f43b1b298f34fbd141585a370e915ebe41503ca1
Panagiotis Ntymenos <pan.ntymenos@gmail.com>
2024-08-24 21:55:59 +0300

    feature(commons-test): Add map utils

---
+++ modules/commons/src/main/kotlin/com/sicklibs/commons/utils/map/MapUtils.kt
@@ -0,0 +1,49 @@
+package com.sicklibs.commons.utils.map
+
+/**
+ * Extension function to filter map entries with null values
+ */
+fun <K, V> Map<K, V>.filterNullValues(): Map<K, V> =
+  filterValues { it == null }
+
+/**
+ * Extension function to map entries with non-null values
+ */
+fun <K, V> Map<K, V?>.filterOutNullValues(): Map<K, V> {
+  val response = mutableMapOf<K, V>()
+
+  mapValues { (key, value) ->
+    if (value != null)
+      response[key] = value
+  }
+
+  return response
+}
+
+/**
+ * Extension function to invert a map
+ */
+fun <K, V> Map<K, V>.invert(): Map<V, K> =
+  map { (key, value) -> value to key }.toMap()
+
+/**
+ * Extension function to convert a map to a list of pairs
+ */
+fun <K, V> Map<K, V>.toPairs(): List<Pair<K, V>> =
+  map { (key, value) -> key to value }
+
+/**
+ * Extension function to filter map entries with duplicate values
+ */
+fun <K, V> Map<K, V>.filterDuplicateValues(): Map<K, V> =
+  filterValues { value ->
+    filterValues { it == value }.size > 1
+  }
+
+/**
+ * Extension function to run a block of code if the map is not empty
+ */
+fun <K, V> Map<K, V>.alsoIfNotEmpty(block: (Map<K, V>) -> Map<K, V>) = also {
+  if (isNotEmpty())
+    block(this)
+}
