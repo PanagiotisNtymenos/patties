58e3ec283afcdfdd6838fb88c3d2f0e1b3d88695
Panagiotis Ntymenos <pan.ntymenos@gmail.com>
2024-08-24 21:52:08 +0300

    feature(commons): Add JaCoCo

--- modules/commons/build.gradle.kts
+++ modules/commons/build.gradle.kts
@@ -5,6 +5,7 @@ version = "0.0.1"
 plugins {
   kotlin("jvm")
   groovy
+  jacoco
 }
 
 repositories {
@@ -20,6 +21,61 @@ dependencies {
   testImplementation(project(":commons-test"))
 }
 
+tasks.check {
+  dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
 tasks.test {
   useJUnitPlatform()
+  finalizedBy(tasks.jacocoTestReport)
+}
+
+val excludedFromJacoco = emptyList<String>()
+
+tasks.jacocoTestReport {
+  reports {
+    xml.required.set(true)
+    html.required.set(true)
+  }
+
+  classDirectories.setFrom(
+    files(
+      classDirectories.files.map {
+        fileTree(it) {
+          exclude(excludedFromJacoco)
+        }
+      }
+    )
+  )
+
+  dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+  classDirectories.setFrom(
+    files(
+      classDirectories.files.map {
+        fileTree(it) {
+          exclude(excludedFromJacoco)
+        }
+      }
+    )
+  )
+
+  violationRules {
+    rule {
+      element = "CLASS"
+
+      limit {
+        counter = "INSTRUCTION"
+        value = "COVEREDRATIO"
+        minimum = 1.toBigDecimal()
+      }
+      limit {
+        counter = "BRANCH"
+        value = "COVEREDRATIO"
+        minimum = 1.toBigDecimal()
+      }
+    }
+  }
 }
